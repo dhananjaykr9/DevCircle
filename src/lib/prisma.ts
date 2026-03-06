bdc2284a69796f140da08f49293aca9abc8a433e
Dhananjay G Kharkar <dkharkar00@gmail.com>
2026-03-06 21:26:40 +0530

    update project

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient({ log: ["error"] });
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: ["error"],
+        datasourceUrl: process.env.DATABASE_URL,
+    });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
